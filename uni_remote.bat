bc05a05a53d4f69bf479c8bce897ba3dd24c42d5
Dion Woolley <woolley.dion@gmail.com>
2017-08-16 02:57:11 +1200

    Update remote script to kill ssh session on remote desktop termination

--- uni_remote.bat
+++ uni_remote.bat
@@ -4,4 +4,8 @@ start /min ssh -L 13389:law110-74.canterbury.ac.nz:3389 linux.cosc.canterbury.ac
 
 ECHO "Connect to remote session"
 
-MSTSC /v localhost:13389 /w:1920 /h:1080 /f
+start /wait MSTSC /v localhost:13389 /w:1920 /h:1080 /f
+
+taskkill /im ssh.exe
+
+exit 0;
