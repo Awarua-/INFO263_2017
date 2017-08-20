6b498a7611734d8dcac23db5f01653bc3c9d491c
Dion Woolley <woolley.dion@gmail.com>
2017-08-20 16:34:30 +1200

    Add Skeleton assignment

---
+++ Project/skeleton/include/config.php
@@ -0,0 +1,7 @@
+<?php //config.php
+$hostname = "";
+$database = "";
+$username = "";
+$password = "";
+$APIKey = "";
+?>
---
+++ Project/skeleton/index.php
@@ -0,0 +1,17 @@
+<?php
+$active = "home";
+require_once 'include/header.php';
+?>
+
+<script async defer
+  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD1dh0Oru6bbn2-m9jyurjupgt6AgJWbyc&callback=initMap">
+</script>
+<script src="scripts/map.js"></script>
+<script>
+    $(document).ready(function() {
+
+    });
+</script>
+<?php
+require_once 'include/footer.php';
+?>
