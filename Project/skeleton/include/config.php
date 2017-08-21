b4a4ed0078d7d7ee314962d1addb93ecd0794356
Dion Woolley <woolley.dion@gmail.com>
2017-08-21 19:45:01 +1200

    Add database default config

--- Project/skeleton/include/config.php
+++ Project/skeleton/include/config.php
@@ -1,7 +1,7 @@
 <?php //config.php
-$hostname = "";
-$database = "";
-$username = "";
-$password = "";
+$hostname = "csse-info263.canterbury.ac.nz";
+$database = "akl_transport";
+$username = "info263";
+$password = "info263";
 $APIKey = ""; # Your API Key here.
 ?>
